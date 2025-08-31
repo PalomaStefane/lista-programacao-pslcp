2fb62f718826aae8e1e290a7aa39c5b160d93a9c
PalomaStefane <hjnloma20@gmail.com>
2025-08-31 16:49:01 -0300

    Atividade 3 - Resolvida

---
+++ AtividadeJava/src/Atividade3.java
@@ -0,0 +1,21 @@
+import java.security.KeyStore;
+
+public class Atividade3 {
+
+    public static void main(String[] args) {
+
+        // 3. Declare: qtdAlunos (inteiro), mediaTurma (real), aprovada (booleano)
+        // e professor (texto). Atribua valores e exiba tudo.
+
+        int qtdAlunos = 20;
+        double mediaTurma = 7.5;
+        boolean aprovada  = true;
+        String professor = "Victor";
+
+        System.out.println("Quantidade de Alunos:" + qtdAlunos);
+        System.out.println("Media da Turma:" + mediaTurma);
+        System.out.println("Aprovados:" + aprovada);
+        System.out.println("Profesor:" + professor);
+
+    }
+}

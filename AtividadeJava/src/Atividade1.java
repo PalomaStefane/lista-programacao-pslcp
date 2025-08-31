6936f86fdc7888905da838ebc554a8014debdd22
PalomaStefane <hjnloma20@gmail.com>
2025-08-31 14:20:23 -0300

    Atividade 1 - Resolvida

---
+++ AtividadeJava/src/Atividade1.java
@@ -0,0 +1,15 @@
+public class Atividade1 {
+    public static void main(String[] args) {
+        String nome = "";
+        String cidade = "";
+        String Pais = "";
+
+        nome = "Paloma Stéfane";
+        cidade = " São Gonçalo do Rio Abaixo ";
+        Pais = " Brasil.";
+
+        System.out.println("Olá, o meu nome é " + nome + ", moro em" + cidade + "no" + Pais);
+
+
+    }
+}

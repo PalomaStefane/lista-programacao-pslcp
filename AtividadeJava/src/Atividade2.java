e7b2cac6a4f2bedda33270d645b422313edbd46a
PalomaStefane <hjnloma20@gmail.com>
2025-08-31 14:42:49 -0300

    Atividade 2 - Resolvida

---
+++ AtividadeJava/src/Atividade2.java
@@ -0,0 +1,21 @@
+import java.awt.print.Printable;
+
+public class Atividade2 {
+    public static void main(String[] args) {
+
+        // 2.Armazene o dia, mês e ano do seu nascimento em três variáveis e apenas exiba cada
+        // uma em linhas separadas.
+
+        int Dia = 20;
+        int Mês =  8;
+        int Ano = 2000;
+
+        System.out.println("Dia:" + 20);
+        System.out.println("Mês:" + 8);
+        System.out.println("Ano:" + 2000);
+
+
+
+
+    }
+}

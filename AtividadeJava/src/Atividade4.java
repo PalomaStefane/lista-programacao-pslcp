b31f35e3f44017ba01623acc87c30dbba21707ba
PalomaStefane <hjnloma20@gmail.com>
2025-09-02 19:05:04 -0300

    Atividade 4 - Resolvida

---
+++ AtividadeJava/src/Atividade4.java
@@ -0,0 +1,13 @@
+public class Atividade4 {
+
+    public static void main(String[] args) {
+
+        //4. Defina uma constante PI = 3.1415 e uma variável raio . Atribua um valor a raio
+        // e exiba “PI = ..., raio = ...”.
+
+        final double PI = 3.1415;
+        double raio = 4;
+        System.out.println("PI = " + PI + " raio = "
+                + raio +".");
+    }
+}

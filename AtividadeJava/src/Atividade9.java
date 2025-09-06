7085568cfce6368002da99b6a53517a613976d99
PalomaStefane <hjnloma20@gmail.com>
2025-09-05 21:06:30 -0300

    Atividade de 9 a 12 resolvidas

---
+++ AtividadeJava/src/Atividade9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Atividade9 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite a base: ");
+        double base = leia.nextDouble();
+        System.out.print("Digite a altura: ");
+        double altura = leia.nextDouble();
+        double perimetro = 2 * (base + altura);
+        System.out.println("O perímetro é: " + perimetro);
+    }
+}
